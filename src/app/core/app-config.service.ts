7721c8faf8183fe8d85e0196a83128745f2b8e14
Naresh Sangana <Naresh.Sangana@likewize.com>
2026-03-04 10:30:17 -0600

    backend-production-615b.up.railway.app

--- src/app/core/app-config.service.ts
+++ src/app/core/app-config.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class AppConfigService {
-  private cfg: any = { apiBaseUrl: 'http://localhost:8000' };
+  private cfg: any = { apiBaseUrl: 'https://backend-production-615b.up.railway.app' };
   get apiBaseUrl() { return this.cfg.apiBaseUrl; }
 }
